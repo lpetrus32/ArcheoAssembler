fce107f5a3eb127f36a0b35f0cf72e0931fcddd1
lpetrus32 <56268582+lpetrus32@users.noreply.github.com>
2020-03-31 16:23:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-
-
-
+//Cache ou affiche le block des filtres
 function show_hideAction(){
     let DataBlock = document.getElementById("DataBlock");
     let BasketBlock = document.getElementById("BasketBlock");
-    let ScreenHeight = document.getElementById("screen").clientHeight;
+    let mainBlockHeight = document.getElementById("mainBlock").clientHeight;
     let ButtonText = document.getElementById("show_hideButton");
 
-    let ParametersBlock = document.getElementById("ParametersBlock");
-    console.log(ScreenHeight);
-    if(ParametersBlock.clientHeight == 0){
+    let FiltersBlock = document.getElementById("FiltersBlock");
+    if(FiltersBlock.clientHeight == 0){
         ButtonText.textContent = "HIDE FILTERS";
-        ParametersBlock.style.display = "block";
-        BasketBlock.style.height = (ScreenHeight*0.8)+'px';
-        DataBlock.style.height = (ScreenHeight*0.8)+'px';
+        FiltersBlock.style.display = "block";
+        BasketBlock.style.height = (mainBlockHeight*0.8)+'px';
+        DataBlock.style.height = (mainBlockHeight*0.8)+'px';
     }else{
         ButtonText.textContent = "SHOW FILTERS";
-        ParametersBlock.style.display = "none";
-        BasketBlock.style.height = ScreenHeight*0.99+'px';
-        DataBlock.style.height = ScreenHeight*0.99+'px';
+        FiltersBlock.style.display = "none";
+        BasketBlock.style.height = (mainBlockHeight-6)+'px'; //largeur bordures = 6px
+        DataBlock.style.height = (mainBlockHeight-6)+'px';
     }
 }
+
 
 let setupListeners = function(){
 
