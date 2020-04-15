f3269db395044dd32e81c23b9251faa1b1f93a65
lpetrus32 <56268582+lpetrus32@users.noreply.github.com>
2020-04-15 19:27:30 +0200

    fonctions de lecture du xml (non fonctionnel)
    problème de lecture de la variable "xml"

--- Codes/index.js
+++ Codes/index.js
@@ -1,3 +1,31 @@
+// Affichage des mignatures
+function handleFiles(files) {
+	var imageType = /^image\//;
+	for (var i = 0; i < files.length; i++) {
+		var file = files[i];
+		if (!imageType.test(file.type)) {
+			alert("veuillez sélectionner une image");
+		}else{
+			if(i == 0){
+				preview.innerHTML = '';
+			}
+			var img = document.createElement("img");
+			img.classList.add("obj");
+			img.file = file;
+			preview.appendChild(img);
+			var reader = new FileReader();
+			reader.onload = ( function(aImg) {
+				return function(e) {
+					aImg.src = e.target.result;
+				};
+			})(img);
+
+			reader.readAsDataURL(file);
+		}
+
+	}
+}
+
 //Cache ou affiche le block des filtres
 function show_hideAction(){
     let DataBlock = document.getElementById("DataBlock");
@@ -37,6 +65,65 @@ function switchAction(){
 
 }
 
+//FONCTIONS POUR RECUPERER LES VALEURS DE CHAQUE ATTRIBUT DE CHAQUE FRAGMENT///// 
+function readXml(xml){
+	let i=0;
+	let lines = [];
+	let newline = "";
+
+	while(i<xml.length){
+		newline="";
+		do{
+		newline+=xml[i];
+		i++;
+		}while(xml.substring(i+1,i+3)[i] != "\n");
+
+		if(!(newline in ["<?xml version=1.0 encoding=ISO-8859-1 ?>","<productData>","</productData>"])){
+			lines.push(newline);
+		}
+		i++;
+	}
+	return lines;
+}
+
+function getXmlAttributes(str) {
+	let i = 2;
+	var attribut = "";
+
+	while(i<str.length){
+		if((str.substring(i-2,i)) == "</"){           
+			while(str[i] != ">"){
+				attribut+=str[i];
+				i++;
+			}
+			attributs.push(attribut);
+			attribut = "";
+		}
+		i++;   
+	}
+	return attributs;
+}
+
+function getXmlValue(str, key) {
+  return str.substring(
+	str.lastIndexOf('<' + key + '>') + ('<' + key + '>').length,
+	str.lastIndexOf('</' + key + '>')
+  );
+}
+
+
+function createObjects(attributs, lignes, list){
+
+	for(let i=0;i<lignes.length;i++){
+
+		let objectValues = [];
+		for(let j=0;j<attributs.length;j++){
+			objectValues.push(getXmlValue(lignes[i], attribut[j]));
+		}
+		list.push(objectValues);
+	}
+}
+
 // conversion est affichage des donner 
 function filePicked(oEvent) {
 	// Get The File From The Input
@@ -61,20 +148,20 @@ function filePicked(oEvent) {
 				table_data += '<tr>';
 				for(var cell_count=0; cell_count<cell_data.length; cell_count++)  // affichage
 				{
-						if(count === 0)
-						{
-							table_data += '<th>'+cell_data[cell_count]+'</th>';
-						}
-						else
-						{
-							table_data += '<td>'+cell_data[cell_count]+'</td>';
-						}
+					if(count === 0)
+					{
+						table_data += '<th>'+cell_data[cell_count]+'</th>';
+					}
+					else
+					{
+						table_data += '<td>'+cell_data[cell_count]+'</td>';
+					}
 				}
 				table_data += '</tr>';
 			}
-		table_data += '</table>'; // table_data : la conversion csv-> tableau html 
-		//console.log(table_data); //// test html 
-		document.getElementById('DataBlock').innerHTML +=table_data; // affichage 
+			table_data += '</table>'; // table_data : la conversion csv-> tableau html 
+			//console.log(table_data); //// test html 
+			document.getElementById('DataBlock').innerHTML +=table_data; // affichage 
 		
 			csvData = csvData.split('\n').map(row => row.trim());  //// conversion csv-> xml 
 			let headings = csvData[1].split(',').map(row => row.trim());
@@ -85,61 +172,67 @@ function filePicked(oEvent) {
 					headings[z]=headings[z].replace("'","");
 					headings[z]=headings[z].replace('°','');
 					headings[z]=headings[z].replace('.','');
-			}}
-			let xml = ``;
-			xml="<?xml version="+"1.0"+" encoding="+"ISO-8859-1"+" ?>\n"                           
+				}
+			}
+
+			var xml = ``;
+			xml="<?xml version="+"1.0"+" encoding="+"ISO-8859-1"+" ?>\n";                           
 			for(let i = 2; i < csvData.length; i++) {
-			let details = csvData[i].split(',').map(row => row.trim())
-			xml += "<productData>\n"
+			let details = csvData[i].split(',').map(row => row.trim());
+			xml += "<productData>\n";
 			
 			for(let j = 0; j < headings.length; j++) {
 				if (headings[j] !== ""){    // condition pour regler le probleme de dimension du tableau 
-			xml += `<${headings[j]}>${details[j]}</${headings[j]}>`
-			}};								
-			xml += "</productData>\n"; // xml : le fichier xml 
-			};
-			console.log(xml)
-	}
-	);
-	
-	
-	
+			xml += `<${headings[j]}>${details[j]}</${headings[j]}>`;
+				}
+			};								
+			xml += "\n</productData>\n"; // xml : le fichier xml 
 			
+			};
+
+			/*
+			//Function to download data to a file
+			function download(data, filename, type) {
+				var file = new Blob([data], {type: type});
+				if (window.navigator.msSaveOrOpenBlob) // IE10+
+					window.navigator.msSaveOrOpenBlob(file, filename);
+				else { // Others
+					var a = document.createElement("a"),
+							url = URL.createObjectURL(file);
+					a.href = url;
+					a.download = filename;
+					document.body.appendChild(a);
+					a.click();
+					setTimeout(function() {
+						document.body.removeChild(a);
+						window.URL.revokeObjectURL(url);  
+					}, 0); 
+				}
+			}*/
+
 		}
+		);
+
+
+
+	/*//creation de la liste de fragments :
+	lignes = readXml(xml);
+
+	attributs = getXmlAttributes(lignes[0]);
+	createObjects(attributs,lignes,Objects); //liste de la liste des valeurs de chaque fragment
+											
+	console.log(Objects[0][1]);	*/
+	}
 // Tell JS To Start Reading The File.. You could delay this if desired
-reader.readAsBinaryString(oFile);}
+reader.readAsBinaryString(oFile);
+}
+
+var attributs = [], lignes=[], Objects=[];
+
+
 
 let setupListeners = function(){
     let datafile = document.getElementById("file");
     datafile.addEventListener('change', filePicked, false);
 }
 window.onload = setupListeners;
-
-
-// Affichage des mignatures
-function handleFiles(files) {
-	var imageType = /^image\//;
-	for (var i = 0; i < files.length; i++) {
-		var file = files[i];
-		if (!imageType.test(file.type)) {
-			alert("veuillez sélectionner une image");
-		}else{
-			if(i == 0){
-				preview.innerHTML = '';
-			}
-			var img = document.createElement("img");
-			img.classList.add("obj");
-			img.file = file;
-			preview.appendChild(img);
-			var reader = new FileReader();
-			reader.onload = ( function(aImg) {
-				return function(e) {
-					aImg.src = e.target.result;
-				};
-			})(img);
-
-			reader.readAsDataURL(file);
-		}
-
-	}
-}
