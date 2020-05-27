bfc90482b955b9fd1471f197bab93160c07f7c55
lpetrus32 <56268582+lpetrus32@users.noreply.github.com>
2020-05-27 20:38:56 +0200

    Add files via upload

---
+++ src/graphs.js
@@ -0,0 +1,17 @@
+/*
+ArcheoAssembler. Project
+M1 BioInformatique
+Projet de programmation
+Référente/Cliente : Ostertag Cécilia
+File 6/6 - GRAPHS MANAGMENT
+*/
+
+var graph = document.getElementById("graphsBlock");
+var size=graph.clientHeight;
+let i = 1;
+
+graph.innerHTML=`<img src="../Data/recon_ost_CC3_it1.png" style="height:100%;position:absolute;clip:rect(0px, ${size}px, ${size}px, 0px);" >`;
+
+
+
+
